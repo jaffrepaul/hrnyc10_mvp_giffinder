6e660772f71596df84f8a7be5df1823bad32d289
paulj <jaffrepaul@gmail.com>
2017-09-15 11:36:45 -0400

    added press release

--- server.js
+++ server.js
@@ -12,15 +12,17 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false })); //get data from form & add boy to req obj
 
 // ========== Routes
-app.get('/', (req, res) => {
+app.get('/gifs', (req, res) => {
   res.sendFile(__dirname + '/index.html')
+  //do the find({}) move from last sprints
 })
 
-app.post('/gifs', (req, res) => {
+app.post('/gif', (req, res) => {
   db.collection('gifs').save(req.body, (err, result) => {
     if (err) console.log(err);
     console.log('saved to db!');
-    res.redirect('/');
+    res.send(result);
+    // res.send('/');
   })
 })
 
