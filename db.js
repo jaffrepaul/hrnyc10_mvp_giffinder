1ab4ef6ab14e5ef9d03cbdad7beb08db3c7998db
paulj <jaffrepaul@gmail.com>
2017-09-15 16:27:58 -0400

    moved static files to /public and set up app to serve it

--- db.js
+++ db.js
@@ -6,5 +6,23 @@ db.once('open', () => console.log('Connected to Mongo!'));
 
 mongoose.connect('mongodb://giffinder:giffinderdb@ds135234.mlab.com:35234/giffinder');
 
+const GifSchema = new mongoose.Schema({ //build schema of Gif
+  query: String,
+  image: String
+});
 
-module.exports = db
+module.exports.Gif = mongoose.model('Gif', GifSchema); //export a Gif model that uses the schema
+
+let save = (obj) => {   // save Gif to the DB
+  return Gif.create({
+    query: query,
+    image: url
+  })
+}
+
+let find = (obj) => {    // find Gif in the DB
+  return Gif.find(obj)
+}
+
+
+module.exports.db = db
--- server.js
+++ server.js
@@ -1,34 +1,78 @@
 // ========== Requires
 const express = require('express');
 const path = require('path');
+// const giphy = require( 'giphy' )( '865215fd0a6e409cad4b6931a17b4ad4' );
+const secrets = require('dotenv').config();
 const bodyParser = require('body-parser');
-const db = require('./db');
+const axios = require('axios');
+const {Gif, db} = require('./db');
 
 // ========== Server
 const app = express();
 
-
 // ========== Middleware
-app.use(bodyParser.urlencoded({ extended: false })); //get data from form & add boy to req obj
+app.use(express.static(path.resolve(__dirname, './public')))
+app.use(bodyParser.urlencoded({ extended: false })); //get data from form & add body to req obj
+app.use(bodyParser.json()) // parse application/json
 
 // ========== Routes
-app.get('/gifs', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
-  //do the find({}) move from last sprints
-})
+// v1:
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/index.html')
+// })
 
-app.post('/gif', (req, res) => {
-  db.collection('gifs').save(req.body, (err, result) => {
-    if (err) console.log(err);
-    console.log('saved to db!');
-    res.send(result);
-    // res.send('/');
+// send user req to server
+app.post('/gif', (req, res) => {  // New entry created (data from Giphy)
+
+  console.log('POST req:', req.body.query); //dog
+
+  // get data from Giphy
+  axios.get(`http://api.giphy.com/v1/gifs/search?q=${req.body.query}&api_key=${process.env.GIPHY_API}&limit=5`)
+  .then(function(response) {
+    Gif.save(req.body)
+    res.send(response);
   })
-})
+});
+
+//   db.collection('gifs').save(req.body, (err, result) => {
+//     if (err) console.log(err);
+//     console.log('saved to db!');
+//     res.send(req.body);
+//     // res.send('/');
+//   })
+// })
+
+app.get('/gifs', function(req, res) { // Returns everything from our DB in an array
+  Gif.find({})
+    .then(function(gifs) {
+      res.send(gifs);
+    });
+  console.log('Got everything from DB');
+});
+
+// ==========getOne, put, delete
+// app.get('/gifs', function(req, res) { // Returns one entry from our DB
+//   Gif.findOne({query: 'dog'})
+//     .exec(function(err, data) {
+//       if (err) {
+//         console.err(err);
+//       } else {
+//         res.send(data)
+//       }
+//     })
+//   res.send('Got one entry from DB');
+// });
+//
+// app.put('/gifs', function(req, res) { // Updates an existing entry
+//   res.send('Entry updated in DB');
+// });
+//
+// app.delete('/gifs', function(req, res) { // Deletes an existing entry
+//   res.send('Entry deleted from DB');
+// });
 
 
-
-
+// ========== Tell server where to listen
 app.listen(3000, function() {
   console.log('listening in port 3000');
 });
